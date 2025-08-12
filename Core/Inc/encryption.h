b13127eacd28360bfdd69e2eab973ba7fc82ba11
LeonUlcakar <67747860+LeonUlcakar@users.noreply.github.com>
2025-08-12 18:15:29 +0200

    added encrzption

---
+++ Core/Inc/encryption.h
@@ -0,0 +1,15 @@
+/*
+ * encryption.h
+ *
+ *  Created on: Aug 12, 2025
+ *      Author: Leon
+ */
+
+#ifndef INC_ENCRYPTION_H_
+#define INC_ENCRYPTION_H_
+
+
+
+
+
+#endif /* INC_ENCRYPTION_H_ */
---
+++ Core/Src/encryption.c
@@ -0,0 +1,15 @@
+/*
+ * encryption.c
+ *
+ *  Created on: Aug 12, 2025
+ *      Author: Leon
+ */
+
+#ifndef INC_ENCRYPTION_C_
+#define INC_ENCRYPTION_C_
+
+
+
+
+
+#endif /* INC_ENCRYPTION_C_ */
